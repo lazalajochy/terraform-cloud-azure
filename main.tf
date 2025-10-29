0731e9010c6ea6b51487ed69a7a188a77ada823a
lazalajochy <lazala1401jochy@outlook.com>
2025-10-29 14:57:04 -0400

    container for deploying backend

--- main.tf
+++ main.tf
@@ -52,3 +52,42 @@ resource "azurerm_container_registry" "acr" {
     owner       = "jochy"
   }
 }
+
+
+# 🧩 Azure Container Instance (para desplegar el backend)
+resource "azurerm_container_group" "nestjs_container" {
+  name                = "nestjs-container"
+  location            = "eastus"
+  resource_group_name = "terraform-cloud-test"
+  os_type             = "Linux"
+
+  container {
+    name   = "nestjs-app"
+    image  = "${azurerm_container_registry.acr.login_server}/common:latest"  # o el tag que usaste en el push
+    cpu    = 1
+    memory = 1.5
+    ports {
+      port     = 3000
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      NODE_ENV = "production"
+    }
+  }
+
+  image_registry_credential {
+    server   = azurerm_container_registry.acr.login_server
+    username = azurerm_container_registry.acr.admin_username
+    password = azurerm_container_registry.acr.admin_password
+  }
+
+  ip_address_type = "Public"
+  dns_name_label  = "nestjscloud-${random_integer.suffix.result}" # genera un subdominio único
+}
+
+# genera un número aleatorio para evitar conflicto con otros dns_name_label
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+}
