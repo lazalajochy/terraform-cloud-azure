9122b58d9e273ddaef83d13176f9234c458441d1
lazalajochy <lazala1401jochy@outlook.com>
2025-10-29 10:41:28 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+# provides configuration details for terraform
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+
+
+# provides configuration details for the azure terraform provider
+
+provider "azurerm" {
+  features {}
+  subscription_id = "147d994b-4b52-4afe-b06f-1465aba9d63e"
+}
+
+
+
+
+# provides the resource group to logically contain resources
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-cloud-test"
+  location = "eastus"
+  tags = {
+    environment = "cloud-test"
+    source      = "terraform"
+    owner       = "jochy"
+
+  }
+}
